7d571cd40eb4ea85e411373ad7bcd09320b2132d
Rahman-Altaf <rahmanch.311@gmail.com>
2024-07-11 00:57:54 +0500

    New branch created

--- start.js
+++ start.js
@@ -1,2 +1,16 @@
-console
-confirm
+function sumEvenNumbers1(start, end) {
+    let sum = 0;
+    
+    for (let i = start; i <= end; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
+const start = 1;
+const end = 10;
+const result = sumEvenNumbers1(start, end);
+
+console.log(`The sum of even numbers between ${start} and ${end} is: ${result}`);
